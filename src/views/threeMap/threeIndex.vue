efc780459cd7fd74b2874727cb9ee2eadd73f8cc
wangcl <wangcl@teleware.cn>
2021-07-20 09:34:12 +0800

    修改：增加three.js

--- src/views/threeMap/threeIndex.vue
+++ src/views/threeMap/threeIndex.vue
@@ -3,6 +3,8 @@
 </template>
 <script>
 import * as THREE from 'three'
+import china from './china.json'
+import * as d3 from 'd3'
 export default {
   data() {
     return {
@@ -16,12 +18,15 @@ export default {
       this.setCamera()
       this.setRenderer()
       this.setLight()
-      const geometry = new THREE.BoxGeometry()
-      const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-      this.cube = new THREE.Mesh(geometry, material)
-      this.scene.add(this.cube)
+      // const geometry = new THREE.BoxGeometry()
+      // const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+      // this.cube = new THREE.Mesh(geometry, material)
+      // this.scene.add(this.cube)
       // this.render()
-      this.animate()
+      // this.animate()
+      this.initMap()
+      this.render()
+      this.addHelper()
     },
     setCamera() {
       this.camera = new THREE.PerspectiveCamera(75, this.width / this.height, 0.1, 1000)
@@ -40,6 +45,10 @@ export default {
       const ambientLight = new THREE.AmbientLight(0xffffff) // 环境光
       this.scene.add(ambientLight)
     },
+    addHelper() {
+      const helper = new THREE.CameraHelper(this.camera)
+      this.scene.add(helper)
+    },
     render() {
       this.renderer.render(this.scene, this.camera)
     },
@@ -48,6 +57,47 @@ export default {
       this.cube.rotation.x += 0.01
       this.cube.rotation.y += 0.01
       this.render()
+    },
+    initMap() {
+      this.map = new THREE.Object3D()
+      console.log(THREE)
+      const projection = d3.geoMercator().center([104.0, 37.5]).scale(80).translate([0, 0])
+      china.features.forEach(val => {
+        const province = new THREE.Object3D()
+        const coordinates = val.geometry.coordinates
+        coordinates.forEach(multiPolygon => {
+          multiPolygon.forEach(polygon => {
+            const shape = new THREE.Shape()
+            const lineMaterial = new THREE.LineBasicMaterial({ color: 0x00ff00 })
+            const lineGeometry = new THREE.Geometry()
+            polygon.forEach((coord, index) => {
+              const [x, y] = projection(coord)
+              if (index === 0) {
+                shape.moveTo(x, -y)
+              }
+              shape.lineTo(x, -y)
+              lineGeometry.vertices.push(new THREE.Vector3(x, -y, 4.01))
+            })
+            const extrudeSettings = {
+              depth: 4,
+              bevelEnabled: false
+            }
+            const geometry = new THREE.ExtrudeGeometry(shape, extrudeSettings)
+            const material = new THREE.MeshBasicMaterial({ color: '#d13a34', transparent: true, opacity: 0.6 })
+            const mesh = new THREE.Mesh(geometry, material)
+            const line = new THREE.Line(lineGeometry, lineMaterial)
+            province.add(mesh)
+            province.add(line)
+          })
+        })
+        province.properties = val.properties
+        if (val.properties.centroid) {
+          const [x, y] = projection(val.properties.centroid)
+          province.properties._centroid = [x, y]
+        }
+        this.map.add(province)
+      })
+      this.scene.add(this.map)
     }
   },
   mounted() {
@@ -58,5 +108,6 @@ export default {
 <style scoped>
 #three-map {
   margin: 0 auto;
+  margin-top: 50px;
 }
 </style>
