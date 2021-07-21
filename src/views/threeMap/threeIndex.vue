577f40badb0ff8a7c393a3b16a523e5edc90af75
wangcl <wangcl@teleware.cn>
2021-07-21 17:48:36 +0800

    修改：处理地图加载bug

--- src/views/threeMap/threeIndex.vue
+++ src/views/threeMap/threeIndex.vue
@@ -5,10 +5,11 @@
 import * as THREE from 'three'
 import china from './china.json'
 import * as d3 from 'd3'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 export default {
   data() {
     return {
-      width: 1000,
+      width: 1500,
       height: 800
     }
   },
@@ -24,13 +25,14 @@ export default {
       // this.scene.add(this.cube)
       // this.render()
       // this.animate()
+      // this.addHelper()
       this.initMap()
       this.render()
-      this.addHelper()
+      this.setController()
     },
     setCamera() {
       this.camera = new THREE.PerspectiveCamera(75, this.width / this.height, 0.1, 1000)
-      this.camera.position.z = 5
+      this.camera.position.z = 50
     },
     // 设置渲染器
     setRenderer() {
@@ -44,6 +46,28 @@ export default {
     setLight() {
       const ambientLight = new THREE.AmbientLight(0xffffff) // 环境光
       this.scene.add(ambientLight)
+    },
+    setController() {
+      this.controls = new OrbitControls(this.camera, this.renderer.domElement)
+      // 动态阻尼系数 就是鼠标拖拽旋转灵敏度，阻尼越小越灵敏
+      this.controls.dampingFactor = 0.5;
+      // 是否可以缩放
+      this.controls.enableZoom = true;
+      //是否自动旋转
+      this.controls.autoRotate = false;
+      // //设置相机距离原点的最近距离
+      // this.controls.minDistance = 20;
+      // //设置相机距离原点的最远距离
+      // this.controls.maxDistance = 1000;
+      //是否开启右键拖拽
+      this.controls.enablePan = true;
+      // //上下翻转的最大角度
+      // this.controls.maxPolarAngle = 1.5;
+      // //上下翻转的最小角度
+      // this.controls.minPolarAngle = 0.0;
+      // 是否可以旋转
+      this.controls.enableRotate = true
+      // this.controls.addEventListener('change', this.render())
     },
     addHelper() {
       const helper = new THREE.CameraHelper(this.camera)
@@ -60,35 +84,49 @@ export default {
     },
     initMap() {
       this.map = new THREE.Object3D()
-      console.log(THREE)
       const projection = d3.geoMercator().center([104.0, 37.5]).scale(80).translate([0, 0])
       china.features.forEach(val => {
         const province = new THREE.Object3D()
         const coordinates = val.geometry.coordinates
         coordinates.forEach(multiPolygon => {
-          multiPolygon.forEach(polygon => {
+          const func = polygon => {
             const shape = new THREE.Shape()
-            const lineMaterial = new THREE.LineBasicMaterial({ color: 0x00ff00 })
-            const lineGeometry = new THREE.Geometry()
+            const lineMaterial = new THREE.LineBasicMaterial({ color: 'white' })
+            const lineGeometry = new THREE.BufferGeometry()
+            const pointArray = []
             polygon.forEach((coord, index) => {
               const [x, y] = projection(coord)
               if (index === 0) {
                 shape.moveTo(x, -y)
               }
               shape.lineTo(x, -y)
-              lineGeometry.vertices.push(new THREE.Vector3(x, -y, 4.01))
+              pointArray.push(new THREE.Vector3(x, -y, 4.01))
             })
+            lineGeometry.setFromPoints(pointArray)
             const extrudeSettings = {
               depth: 4,
               bevelEnabled: false
             }
             const geometry = new THREE.ExtrudeGeometry(shape, extrudeSettings)
-            const material = new THREE.MeshBasicMaterial({ color: '#d13a34', transparent: true, opacity: 0.6 })
-            const mesh = new THREE.Mesh(geometry, material)
+            const material = new THREE.MeshBasicMaterial({ color: '#2defff', transparent: true, opacity: 0.6 })
+            const material1 = new THREE.MeshBasicMaterial({
+              color: '#3480C4',
+              transparent: true,
+              opacity: 0.5,
+            })
+            const mesh = new THREE.Mesh(geometry, [material, material1])
             const line = new THREE.Line(lineGeometry, lineMaterial)
             province.add(mesh)
             province.add(line)
-          })
+          }
+          if (multiPolygon.length > 1) {
+            func(multiPolygon)
+          } else {
+            multiPolygon.forEach(polygon => {
+              func(polygon)
+            })
+          }
+
         })
         province.properties = val.properties
         if (val.properties.centroid) {
